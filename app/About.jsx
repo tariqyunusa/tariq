1b4c9860566b9718aef9d7d1de60f41588357368
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-02-27 22:26:03 +0100

    added popup to about section

--- app/About.jsx
+++ app/About.jsx
@@ -1,11 +1,11 @@
 import styles from './styles/About.module.css'
-
+import Popup from './components/Popup'
 export default function About() {
     return(
         <section className={styles.about__section}>
             <div className={styles.about__section_text__wrapper}>
                 <h1 className={styles.about__header}>
-                I am someone who enjoys being engaged in the creative process so when i am not writing code i curate playlists and podcasts, captures natures essence, read a lot, write sometimes and even though i am not a conventional designer i experiment with layouts and typography.
+                I am someone who enjoys being engaged in the creative process so when i am not writing code i curate <Popup>playlists</Popup> and <Popup>podcasts</Popup>, <Popup>captures</Popup> natures essence, <Popup>read alot</Popup>, <Popup>write sometimes</Popup> and even though i am not a conventional designer i experiment with layouts and typography.
                 </h1>
             </div>
         </section>
