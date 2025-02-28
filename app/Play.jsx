229a1b494f9194fb55bb2cb190e97b1ea60d96a6
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-02-28 23:55:58 +0100

    fix

--- app/Play.jsx
+++ app/Play.jsx
@@ -1,12 +1,14 @@
+import Carousel from './components/Carousel'
+import Popup from './components/Popup'
 import styles from './styles/Play.module.css'
 export default function Play() {
     return(
         <section className={styles.play}>
         <div className={styles.play__wrapper}>
             <h1 className={styles.play__text_header}>Playground</h1>
-            <h1 className={styles.play__text__paragraph}>A brief showcase of my experiments i do when i am trying to figure things out, learn or just have fun. see all </h1>
+            <h1 className={styles.play__text__paragraph}>A brief showcase of my experiments i do when i am trying to figure things out, learn or just have fun. <Popup>See all</Popup> </h1>
         </div>
-
+        <Carousel />
         </section>
     )
 }
---
+++ app/components/Carousel.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Carousel.module.css'
+export default function Carousel() {
+    return(
+        <div className={styles.Carousel__wrapper}>
+            <div className={styles.Carousel}>
+                
+            </div>
+            <div></div>
+        </div>
+    )
+}
