0a8ae7de46c82f8f333181327680358205b544c2
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-03-12 11:12:20 +0100

    animation: animated /projects

--- app/Projects/page.js
+++ app/Projects/page.js
@@ -1,9 +1,13 @@
+"use client"
+import { useEffect } from "react"
 import styles from "../styles/project_section.module.css"
+import { textReveal } from "../utils/Animations"
 export default function Projects() {
+    useEffect(() => {textReveal()},[])
     return(
         <section className={styles.projects__section_page}>
             <div className={styles.projects__section_wrapper}>
-                <h1>
+                <h1 data-animation = 'header'>
                 🏗️This Page is still under contruction
                 </h1>
             </div>
