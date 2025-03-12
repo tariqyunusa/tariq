2e87f87d58000532df4fd9f90c55a5c6db3bc5fb
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-03-12 16:52:16 +0100

    fix: modified pre-loader staircase)

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import styles from "../styles/Loader.module.css";
+import gsap from "gsap";
 
 const Loader = ({ setLoading }) => {
   const [progress, setProgress] = useState(0);
@@ -11,17 +12,42 @@ const Loader = ({ setLoading }) => {
 
     const updateProgress = () => {
       if (progressValue < 100) {
-        progressValue += Math.random() * 10; 
+        progressValue += Math.random() * 10;
         setProgress(Math.min(100, Math.floor(progressValue)));
         requestAnimationFrame(updateProgress);
       }
     };
 
     const handleLoad = () => {
-      setProgress(100);
-      setTimeout(() => setLoading(false), 500); 
-    };
-
+        const tl = gsap.timeline();
+        tl.to(
+          ".loader__filler",
+          { y: "0%", stagger: 0.2, ease: "power4.out", duration: 1.5 }
+        )
+          .add(() => {
+            gsap.to({}, {
+              duration: 2, 
+              onUpdate: function () {
+                setProgress(prev => (prev < 100 ? prev + 1 : 100));
+              },
+              onComplete: () => {
+                gsap.to(".loader__progress", { y: "100%", opacity: 0, duration: 0.5 });
+              }
+            });
+          })
+        
+          .to(".loader__filler", {
+            y: "-100%",
+            stagger: { each: 0.2, from: "end" }, 
+            ease: "power4.inOut",
+            duration: 1.5,
+          },) 
+          .to(".loader__section", {
+            opacity: 0,
+            duration: 0.5,
+            onComplete: () => setLoading(false),
+          });
+      };
     if (document.readyState === "complete") {
       handleLoad();
     } else {
@@ -36,10 +62,11 @@ const Loader = ({ setLoading }) => {
 
   return (
     <section className={styles.loader__section}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className={`${styles.loader__filler} loader__filler`}></div>
+      <div className={`${styles.loader__filler} loader__filler`}></div>
+      <div className={`${styles.loader__filler} loader__filler`}></div>
+      <div className={`${styles.loader__filler} loader__filler`}></div>
+      <h1 className={`${styles.loader__progress} loader__progress`}>{progress}%</h1>
     </section>
   );
 };
