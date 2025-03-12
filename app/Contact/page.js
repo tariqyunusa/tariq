233a712767a4bfa5decd42716d54213c0c37e020
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-03-12 11:17:43 +0100

    animation: animated /contact

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -1,15 +1,21 @@
+"use client"
+import { useEffect } from 'react';
 import styles from '../styles/Footer.module.css'
 import { FiGithub, FiInstagram, FiLinkedin,  FiTwitter } from "react-icons/fi";
+import { textReveal } from '../utils/Animations';
 
 export default function contact() {
+  useEffect(() => {
+    textReveal()
+  },[])
     return(
         <footer className={styles.footer}>
      <div className={styles.footer_redundant}>
      <div className={styles.footer__wrapper}>
-        <h1>Got a question, offer, idea in mind ? mail me lets talk further.</h1>
+        <h1 data-animation = 'header'>Got a question, offer, idea in mind ? mail me lets talk further.</h1>
       </div>
      </div>
-      <div className={styles.footer__links}>
+      <div className={styles.footer__links} data-animation = 'header'>
       <a href="https://github.com/tariqyunusa" target='_blank' rel='noopener noreferrer'><FiGithub /></a>
       <a href="https://www.instagram.com/tariikh_" target='_blank' rel='noopener noreferrer'><FiInstagram /></a>
       <a href="https://www.linkedin.com/in/tariq-yunusa-a5414b248/" target='_blank' rel='noopener noreferrer'><FiLinkedin /></a>
