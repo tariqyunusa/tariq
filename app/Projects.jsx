c5099d196244b30185470dcabba2a88b5e5ba90c
Tariq Yunusa <tariikhyunusa@gmail.com>
2025-07-27 14:04:27 +0100

    modified projects

--- app/Projects.jsx
+++ app/Projects.jsx
@@ -4,13 +4,10 @@ import styles from './styles/Projects.module.css'
 export default function Projects() {
    
     const works =  [
-        {name: "GL", stack: ["Full-Stack Develoment", "Design"], alt: "GL", link: "See Live", route: "https://gl-blog-qiro.vercel.app/"}, 
-        {name: "Campaign", stack: ["Front-End Develoment", "Design", "Creative Direction"],alt: "Campaign", link: "See Live", route: "https://campaign-chi.vercel.app/"},
-        {name: "Campaign-V2", stack: ["Front-End Develoment", "Design", "Creative Direction"], alt: "Campaign-V2", link: "See Live", route: "https://campaign-v2.vercel.app/"},
-        {name: "Node Server", stack: ["Back-End develoment"], alt: "Node Server", link: "See Live", route: "https://github.com/tariqyunusa/JSON-webserver"},
-        {name: "Samsung Showcase", stack: ["Front-End Develoment", "Design", "Creative Direction"], alt: "Samsung Showcase", link: "See Live", route: "https://samsung-s24-showcase.vercel.app/"},
+        {name: "LVI Media", stack: ["Front-End Develoment"],alt: "LVI Media", link: "See Live", route: "https://lvi-media-one.vercel.app/"},
+        {name: "Sophie's Eclat", stack: ["Front-End Develoment", ], alt: "Sophie's Eclat", link: "See Live", route: "https://sophie-clat.vercel.app/"}, 
+        {name: "Campaign-V2", stack: ["Front-End Develoment", "Design"], alt: "Campaign-V2", link: "See Live", route: "https://campaign-v2.vercel.app/"},
         {name: "Tone Hunter", stack: ["Front-End Development"], alt: "Tone Hunter", link: "See Live", route: "https://www.npmjs.com/package/tone-hunter"},
-        {name: "Youtube Downloader", stack: ["Scripting"], alt: "Youtube Downloader", link: "See Live", route: "https://github.com/tariqyunusa/youtube-downloader.git"},
     ]
     return(
     
