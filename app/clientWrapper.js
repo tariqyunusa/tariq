8596ced741de754744de99dd2c14024a69454c65
Tariq Yunusa <tariikhyunusa@gmail.com>
2026-02-16 17:19:00 +0100

    added GA tracking

--- app/clientWrapper.js
+++ app/clientWrapper.js
@@ -1,19 +1,48 @@
 "use client";
+
 import { useEffect, useState } from "react";
 import Loader from "./components/Loader";
 import Nav from "./components/Nav";
-
+import Script from "next/script";
+import { usePathname } from "next/navigation";
 
 export default function ClientWrapper({ children }) {
-  const [isLoading, setIsLoading] = useState(true);
- 
+  const [isLoading, setLoading] = useState(true);
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
+  const pathname = usePathname();
 
+  useEffect(() => {
+    if (!GA_ID || !window.gtag) return;
+    window.gtag('config', GA_ID, {
+      page_path: pathname,
+    });
+  }, [pathname, GA_ID]);
 
   return (
     <>
+      
+      {GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+        </>
+      )}
+
       {isLoading ? (
-        <Loader setLoading={setIsLoading} />
+        <Loader setLoading={setLoading} />
       ) : (
         <>
           {children}
--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  
   return (
     <html lang="en">
       <body> 
